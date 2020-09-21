d95738afb2a6af427bc783dcedf8b3c26d51ec54
Kirill Vlasenkov <vlasenkov.kv@phystech.edu>
2020-09-22 02:53:15 +0300

    added parent.sh and current.sh

---
+++ w2/current.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mkdir ./"hello current"/
+echo "hello current!!" > ./"hello current"/current.txt
+cat ./"hello current"/current.txt
---
+++ w2/parent.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mkdir ../"hello parent"/
+echo "hello parent!!" > ../"hello parent"/parent.txt
+cat ../"hello parent"/parent.txt
