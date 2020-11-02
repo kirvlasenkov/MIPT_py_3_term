23f164b6ed114eec3d42819280726fd427eef4a7
Kirill <vlasenkov.kv@phystech.edu>
2020-11-02 15:18:38 +0300

    almost fully completed week 7

---
+++ w7/my_mathematics/simple_math.py
@@ -0,0 +1,48 @@
+from typing import Tuple, Union
+import math
+import cmath
+import numpy as np
+
+class MyMath:
+    '''
+    Simple class with some
+    mathematical operations.
+    '''
+    pi: float = 3.14
+    _complex: bool = False  # Инкапсуляция
+
+    @staticmethod
+    def sin(x: Union[int, float]) -> Union[int, float]:
+        '''
+        Copy of standard sine function.
+        Just need to demonstrate static method philosophy.
+        :param x: definite point
+        :return: sin(x)
+        '''
+        return np.sin(x)
+
+    @classmethod
+    def complex(cls):
+        return cls._complex
+
+    @classmethod
+    def sqrt(cls, x: Union[int, float]) -> Union[Union[int, float],
+                                                 Tuple[Union[int, float], Union[int, float]]]:
+
+        if x < 0:
+            if cls.complex() is False:  # Элементы полиморфизма
+                raise ValueError("Incorrect number! You are located in Real field")
+            else:
+                result: complex = cmath.sqrt(x)
+                return result.real, result.imag
+
+        else:
+            return math.sqrt(x)
+
+
+class MyComplexMath(MyMath):  # Наследование
+    '''
+    Variation of MyMath for complex field.
+    '''
+    _complex: bool = True  # Наследование и инкапсуляция
+
---
+++ w7/tests/test_my_math.py
@@ -0,0 +1,20 @@
+from my_mathematics.simple_math import MyMath
+import math
+import pytest
+
+
+@pytest.mark.parametrize('x', [0, 1, 2, 3, 4, 5, 0.01, 3e-7, 232, 213123, 392, 921])
+def test_check_sine(x):
+    assert math.sin(x) == MyMath.sin(x)
+
+def test_check_pi():
+    assert round(math.pi, 2) == MyMath.pi
+
+@pytest.mark.parametrize('x', [0, 1, 2, 3, 4, 5, 0.01, 3e-7, 232, 213123, 392, 921])
+def test_check_sqrt(x):
+    assert math.sqrt(x) == MyMath.sqrt(x)
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('x', [-1, -4, -5, -2])
+def test_neg_sqrt(x):
+    assert math.sqrt(x) == MyMath.sqrt(x)
