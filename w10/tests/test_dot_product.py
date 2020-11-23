db371e2cd3e232fade43a24173b8ff2ba917737f
Kirill <vlasenkov.kv@phystech.edu>
2020-11-24 02:24:06 +0300

    added tests for the dot product

---
+++ w10/tests/test_dot_product.py
@@ -0,0 +1,40 @@
+import pytest
+
+from w10.hw_scripts.dot_product import dot_product
+from my_mathematics.linear_algebra import MyVector
+
+
+@pytest.mark.parametrize('lhs, rhs', [(MyVector(3, 2, 3, 4), MyVector(3, 0, 6, 4)),
+                                      (MyVector(2, 3, 4), MyVector(2, 1, 0)),
+                                      (MyVector(2, 3, 4), MyVector(2, 1, 0)),
+                                      (MyVector(1, 9), MyVector(1, 2)),
+                                      (MyVector(5, 2, 1, 3, 4, 5), MyVector(5, 4, 10, 6, 3, 4))
+                                      ]
+                         )
+def test_dot_product_on_multithreading(lhs, rhs):
+    assert round(dot_product(lhs, rhs, mode="multithreading", units_number=1)[0], 5) == \
+           round(lhs.args_as_np.dot(rhs.args_as_np), 5)
+
+    assert round(dot_product(lhs, rhs, mode="multithreading", units_number=2)[0], 5) == \
+           round(lhs.args_as_np.dot(rhs.args_as_np), 5)
+
+    assert round(dot_product(lhs, rhs, mode="multithreading", units_number=4)[0], 5) == \
+           round(lhs.args_as_np.dot(rhs.args_as_np), 5)
+
+
+@pytest.mark.parametrize('lhs, rhs', [(MyVector(3, 2, 3, 4), MyVector(3, 0, 6, 4)),
+                                      (MyVector(2, 3, 4), MyVector(2, 1, 0)),
+                                      (MyVector(2, 3, 4), MyVector(2, 1, 0)),
+                                      (MyVector(1, 9), MyVector(1, 2)),
+                                      (MyVector(5, 2, 1, 3, 4, 5), MyVector(5, 4, 10, 6, 3, 4))
+                                      ]
+                         )
+def test_dot_product_on_multiprocessing(lhs, rhs):
+    assert round(dot_product(lhs, rhs, mode="multiprocessing", units_number=1)[0], 5) == \
+           round(lhs.args_as_np.dot(rhs.args_as_np), 5)
+
+    assert round(dot_product(lhs, rhs, mode="multiprocessing", units_number=2)[0], 5) == \
+           round(lhs.args_as_np.dot(rhs.args_as_np), 5)
+
+    assert round(dot_product(lhs, rhs, mode="multiprocessing", units_number=4)[0], 5) == \
+           round(lhs.args_as_np.dot(rhs.args_as_np), 5)
